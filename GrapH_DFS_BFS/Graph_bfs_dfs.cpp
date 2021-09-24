a099f2a84ed3d020b047cab8c67a9df1e962a8ec
minaev-aa <71834110+minaev-aa@users.noreply.github.com>
2021-09-25 00:51:45 +0300

    Update Graph_bfs_dfs.cpp

--- GrapH_DFS_BFS/Graph_bfs_dfs.cpp
+++ GrapH_DFS_BFS/Graph_bfs_dfs.cpp
@@ -13,54 +13,54 @@ public:
 	Graph()
 	{
         count = 0;
-    }
+    	}
 
 	Graph(std::vector <std::vector <int>> init_matr)
 	{
-        int size = init_matr.size();
-        bool type = true;
-        for (int i = 0; i < size; i++)
+		int size = init_matr.size();
+		bool type = true;
+		for (int i = 0; i < size; i++)
 		{
-            if (size != init_matr[i].size())
+			if (size != init_matr[i].size())
 			{
-                type = false;
-            }
-        }
-        if (type == true)
+			type = false;
+			}
+		}
+		if (type == true)
 		{
-            count = size;
-            matrix.insert(matrix.begin(), init_matr.begin(), init_matr.end());
-        }
-    }
+		    count = size;
+		    matrix.insert(matrix.begin(), init_matr.begin(), init_matr.end());
+		}
+    	}
 	int getSize()
 	{
-        return count;
-    }
+        	return count;
+    	}
 
 	std::vector<std::vector <int>> getMatrix()
 	{
-        return matrix;
-    }
+        	return matrix;
+   	}
 
 	void printMatrix()
 	{
         for (int i = 0; i < count; i++)
+	{
+        	for (int j = 0; j < count; j++)
 		{
-            for (int j = 0; j < count; j++)
+                	if (matrix[i][j] == 1)
 			{
-                if (matrix[i][j] == 1)
-				{
-                    std::cout << 1 << " ";
-                }
-            	else 
-				{
-                    std::cout << 0 << " ";
-                }
-            }
-            std::cout << std::endl;
+                    		std::cout << 1 << " ";
+                	}
+            		else 
+			{
+                    		std::cout << 0 << " ";
+                	}
+            	}
+            	std::cout << std::endl;
         }
-		std::cout << count << std::endl;
-    }
+	std::cout << count << std::endl;
+    	}
 };
 
 
@@ -69,7 +69,7 @@ class DFS
 private:
     std::vector <char> color; //w - white, b - black, g - gray;
     std::vector <int> p;
-
+	
     void depth_first_search(int start, Graph &G)
 	{
         discover_vertex(start, G);
@@ -85,14 +85,14 @@ private:
 	
 	void initialize_vertex(int num, Graph &G)
 	{
-        color[num] = 'w';
-        p[num] = num;
-    }
+        	color[num] = 'w';
+       	 	p[num] = num;
+    	}
 
     void start_vertex(int num, Graph &G)
-	{
+    {
         if (color[num] == 'w')
-		{
+	{
             depth_first_search(num, G);
         }
     }
@@ -103,22 +103,23 @@ private:
     }
 
     void examine_edge(int from, int to, Graph &G)
-	{
+   {
         if (color[to] == 'w')
-		{
+	{
             tree_edge(from, to, G);
         }
     	if (color[to] == 'g')
-		{
+	{
             back_edge(from, to, G);
         }
     	if (color[to] == 'b')
-		{
+	{
             forward_or_cross_edge(from, to, G);
         }
     }
 
-    void tree_edge(int from, int to, Graph &G){
+    void tree_edge(int from, int to, Graph &G)
+    {
         p[to] = from;
         start_vertex(to, G);
     }
@@ -130,16 +131,17 @@ private:
     void finish_edge(int from, int to, Graph &G){return;}
 
     void finish_vertex(int num, Graph &G)
-	{
+    {
         color[num] = 'b';
     }
 
 public:
     DFS(Graph &G, int start)
-	{
-		p = std::vector<int>(G.getSize());
+    {
+	p = std::vector<int>(G.getSize());
         color = std::vector<char>(G.getSize());
-        for (int i = 0; i < G.getSize(); i++){
+        for (int i = 0; i < G.getSize(); i++)
+	{
             initialize_vertex(i, G);
         }
         start_vertex(start, G);
@@ -149,11 +151,12 @@ public:
         return color;
     }
 
-    ~DFS(){
-        color.clear();
-		color.shrink_to_fit();
+    ~DFS()
+    {
+    	color.clear();
+    	color.shrink_to_fit();
         p.clear();
-		p.shrink_to_fit();
+	p.shrink_to_fit();
     }
 };
 
@@ -186,43 +189,43 @@ private:
     std::vector <int> p;
 	std::vector <int> d;
 	std::queue<int> q;
-    void depth_first_search(int start, Graph &G)
+    void breadth_first_search(int start, Graph &G)
 	{
-		int rez = start;
-		d[start] = 0;
+	int rez = start;
+	d[start] = 0;
         discover_vertex(start, G);
-		while(q.empty()==false)
+	while(q.empty()==false)
+	{
+		int start = q.front();
+		q.pop();
+		for (int i = 0; i < G.getSize(); i++)
 		{
-			int start = q.front();
-			q.pop();
-	        for (int i = 0; i < G.getSize(); i++)
+			if (G.getMatrix()[start][i] == 1)
 			{
-	            if (G.getMatrix()[start][i] == 1)
-				{
-	                examine_edge(start, i, G);
-	            }
-	        }
-	        finish_vertex(start, G);
+			examine_edge(start, i, G);
+		   	}
 		}
-		std::cout<<"BFS len to all node from "<<rez<<":";
-		for(int z = 0; z < d.size(); z++)
-		{
-			std::cout<<" "<<d[z]<<" ";
-		}
-		std::cout<<std::endl;
-    }
+		finish_vertex(start, G);
+	}
+	std::cout<<"BFS len to all node from "<<rez<<":";
+	for(int z = 0; z < d.size(); z++)
+	{
+		std::cout<<" "<<d[z]<<" ";
+	}
+	std::cout<<std::endl;
+    	}
 	
 	void initialize_vertex(int num, Graph &G)
 	{
         color[num] = 'w';
 		d[num]= -1;
         p[num] = num;
-    }
+    	}
 
     void start_vertex(int num, Graph &G)
-	{
+    {
         if (color[num] == 'w')
-		{
+	{
             depth_first_search(num, G);
         }
     }
@@ -230,28 +233,29 @@ private:
     void discover_vertex(int num, Graph &G)
 	{
         color[num] = 'g';
-		q.push(num);
-    }
+	q.push(num);
+        }
 
     void examine_edge(int from, int to, Graph &G)
 	{
         if (color[to] == 'w')
-		{
+	{
             tree_edge(from, to, G);
         }
     	if (color[to] == 'g')
-		{
+	{
             back_edge(from, to, G);
         }
     	if (color[to] == 'b')
-		{
+	{
             forward_or_cross_edge(from, to, G);
         }
     }
 
-    void tree_edge(int from, int to, Graph &G){
+    void tree_edge(int from, int to, Graph &G)
+    {
         p[to] = from;
-		d[to] = d[from] + 1;
+	d[to] = d[from] + 1;
         discover_vertex(to, G);
     }
 
@@ -269,21 +273,23 @@ private:
 
 public:
     BFS(Graph &G, int start)
-	{
-		p = std::vector<int>(G.getSize());
-		d = std::vector<int>(G.getSize());
+    {
+	p = std::vector<int>(G.getSize());
+	d = std::vector<int>(G.getSize());
         color = std::vector<char>(G.getSize());
-        for (int i = 0; i < G.getSize(); i++){
+        for (int i = 0; i < G.getSize(); i++)
+	{
             initialize_vertex(i, G);
         }
         start_vertex(start, G);
     }
 
-    ~BFS(){
+    ~BFS()
+    {
         color.clear();
-		color.shrink_to_fit();
+	color.shrink_to_fit();
         p.clear();
-		p.shrink_to_fit();
+	p.shrink_to_fit();
     }
 };
 
@@ -292,18 +298,21 @@ int _tmain(int argc, _TCHAR* argv[])
 	int n;
 	std::cin >> n;
 	std::vector <std::vector <int>> R;
-    for (int i = 0; i < n; i++){
-        R.push_back(std::vector<int> (n));
-    }
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            int type;
-            std::cin >> type;
-            R[i][j] = type;
-        }
-}
-    Graph G(R);
-	 BFS len(G,6);
+    	for (int i = 0; i < n; i++)
+	{
+        	R.push_back(std::vector<int> (n));
+    	}
+    	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+		    int type;
+		    std::cin >> type;
+		    R[i][j] = type;
+        	}
+	}
+   	Graph G(R);
+	BFS len(G,6);
 	G.printMatrix();
 	find_comps(G);
 	system("pause");
